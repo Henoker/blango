75f521643c114fe0b45d487cd6672dd217b66d93
Henock Tesfatsion <hennybany@gmail.com>
2024-02-14 17:00:57 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from blog.models import Post
-from django.shortcuts import redirect
 from blog.forms import CommentForm 
 import logging
+
 
 # Logging
 logger = logging.getLogger(__name__)
 
 # Create your views here.
-def index(request): 
-    # Returns Post title, Author, Datetime
-    posts = Post.objects.filter(published_at__lte=timezone.now()).select_related("author")
-    # Log quantity of posts
-    logger.debug("Got %d posts", len(posts))
 
+def index(request):
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    logger.debug("Got %d posts", len(posts))
     return render(request, "blog/index.html", {"posts": posts})
 
 def post_detail(request, slug):
